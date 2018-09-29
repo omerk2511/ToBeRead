731696c0f33d83c02e9baaa8f26da38e97f4a124
Omer Katz <omerk2511@gmail.com>
2018-09-29 17:28:35 +0300

    Added an option to open an item from the list.

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,5 @@
 $(function(){
     init();
-
     $('#add-to-list').click(addToList);
 });
 
@@ -11,7 +10,7 @@ function init(){
             readingList = JSON.parse(data.ReadingList);
 
         clearReadingList();
-        displayReadingList(readingList);        
+        displayReadingList(readingList);
     });
 }
 
@@ -23,12 +22,16 @@ function displayReadingList(readingList){
     for(var i = 0; i < readingList.length; i++){
         if(readingList[i] && readingList[i].url && readingList[i].title){
             var itemToAdd = '<button type="button" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center open-item" id="'
-                + readingList[i].url + '">' + readingList[i].title + '<span class="badge badge-danger badge-pill delete-item" id="'
+                + readingList[i].url + '">' + readingList[i].title + '<span class="badge badge-danger badge-pill delete-item" id="x'
                 + readingList[i].url + '">X</span></button>';
 
             $('#reading-list').append(itemToAdd);
         }
     }
+
+    $('.list-group-item').each(function(){
+        $(this).click(openItem);
+    });
 }
 
 function addToList(event){
@@ -48,5 +51,27 @@ function addToList(event){
                 chrome.storage.local.set({ 'ReadingList': JSON.stringify(readingList) }, init);
             }
         });
+    });
+}
+
+function openItem(event){
+    var url = event.currentTarget.id;
+    removeItem(url, function(){
+        chrome.tabs.create({ url: url, active: true });
+    });
+}
+
+function removeItem(url, callback){
+    chrome.storage.local.get('ReadingList', function(data){
+        var readingList = [];
+        if(data.ReadingList)
+            readingList = JSON.parse(data.ReadingList);
+
+        for(var i = 0; i < readingList.length; i++){
+            if(readingList[i].url == url){
+                readingList.splice(i, 1);
+                chrome.storage.local.set({ 'ReadingList': JSON.stringify(readingList) }, callback);
+            }
+        }
     });
 }
